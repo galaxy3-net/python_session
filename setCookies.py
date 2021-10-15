f59620f48e48eeff68d5ada3bffb786f32ca3ad2
korbenkirscht <korben@kirscht.com>
2021-10-14 20:18:23 -0700

    S1

--- setCookies.py
+++ setCookies.py
@@ -4,7 +4,7 @@ import requests
 
 setCookieURL = "https://httpbin.org/cookies"
 
-cookies = {'location': 'California'}
+cookies = {'location': 'Colorado'}
 
 response = requests.get(setCookieURL, cookies=cookies)
 #print(response.text)
