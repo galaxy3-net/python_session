3826c58d69bc2d84dff880515b246f2e0f3cc5ec
korbenkirscht <korben@kirscht.com>
2021-10-14 20:13:30 -0700

    S1

--- setCookies.py
+++ setCookies.py
@@ -8,10 +8,12 @@ cookies = {'location': 'California'}
 
 response = requests.get(setCookieURL, cookies=cookies)
 print(response.text)
+print(response.cookies)
 
 setCookieURL = 'https://google.com'
 cookieName = '1P_JAR'
 
 response = requests.get(setCookieURL)
 print(response.cookies[cookieName])
+print(response.text)
 print(response.cookies)
