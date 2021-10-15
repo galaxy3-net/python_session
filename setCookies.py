f894277004e34ee072f14244626c20a624286371
korbenkirscht <korben@kirscht.com>
2021-10-14 20:35:59 -0700

    S1

--- setCookies.py
+++ setCookies.py
@@ -18,7 +18,7 @@ cookieName = '1P_JAR'
 
 response = requests.get(setCookieURL)
 print('Request 2: response.cookees[' + cookieName + "]\n" +  response.cookies[cookieName])
-f.write(response.text)
+report.write(response.text)
 #print(response.text)
 print('Request 2: response.cookies' + "\n")
 print(response.cookies)
